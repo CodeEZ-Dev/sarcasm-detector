00ae5e146bd41c79c2d96cf9337a48bb3d9f033b
Muralidharan <muralidharan1307@gmail.com>
2025-04-20 19:18:25 +0530

    Inital Commit

---
+++ client/src/App.js
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState(0);
+  const [submit, setSubmit] = useState(false);
+
+  useEffect(() => {
+    const postData = async () => {
+      axios
+        .post("http://localhost:8000/api/", {
+          headline: query,
+        })
+        .then((response) => {
+          // console.log(response.data);
+          setResult(response.data.message);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
+
+    postData();
+  }, [submit]);
+
+  const handleChange = (e) => {
+    // console.log(`query: ${query}`);
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setSubmit(!submit);
+  };
+
+  return (
+    <div
+      className="container-contact100"
+      style={{ backgroundImage: "url('images/bg-01.jpg')" }}
+    >
+      <div className="wrap-contact100">
+        <form className="contact100-form validate-form">
+          <span className="contact100-form-title">
+            Input a headline to check if it's sarcastic or not!
+          </span>
+
+          <div
+            className="wrap-input100 validate-input"
+            data-validate="Message is required"
+          >
+            <span className="label-input100">News Headline</span>
+            <textarea
+              className="input100"
+              name="headline"
+              placeholder="Enter Headline Here..."
+              value={query}
+              onChange={(event) => handleChange(event)}
+            ></textarea>
+          </div>
+
+          <div className="wrap-input100">
+            <span className="label-input100">Result</span>
+            <input
+              style={{ color: result ? "red" : "green" }}
+              className="input100"
+              type="text"
+              name="web"
+              placeholder="Result"
+              value={
+                result
+                  ? "I think this is a sarcastic headline"
+                  : "I don't think this is a sarcastic headline"
+              }
+            />
+          </div>
+
+          <div className="container-contact100-form-btn">
+            <div className="wrap-contact100-form-btn">
+              <div className="contact100-form-bgbtn"></div>
+              <button
+                type="button"
+                className="contact100-form-btn"
+                onClick={() => handleSubmit()}
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <span className="contact100-more">
+        Powered by React, Django and Tensorflow-Keras
+      </span>
+    </div>
+  );
+};
+
+export default App;
---
+++ server/form/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+from tensorflow.keras.models import load_model
+import numpy as np
+import pickle
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+model = load_model('/Users/munusami/Desktop/2ndsemprojects/NLP_project/sarcasm-detector/sarcasm_model.h5')
+
+with open('/Users/munusami/Desktop/2ndsemprojects/NLP_project/sarcasm-detector/tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+
+def home(request):
+    return HttpResponse(json.dumps({'message': 'Homepage', 'status': 200}))
+
+
+@csrf_exempt
+def api(request):
+    # request_body = request.body.decode("utf-8")if request.method == 'POST':
+    if request.method == 'POST':
+        request_body = json.loads(request.body.decode("utf-8"))
+        print(request_body)
+
+        sentence = [request_body['headline']]
+        sequences = tokenizer.texts_to_sequences(sentence)
+        padded = pad_sequences(sequences, maxlen=100,
+                               padding='post', truncating='post')
+        pred_temp = model.predict(padded)
+        pred = np.round(pred_temp[0][0])
+        print(pred)
+
+        return HttpResponse(json.dumps({'route': 'api route POST', 'message': int(pred)}))
+    else:
+        return HttpResponse(json.dumps({'message': 'api route GET'}))
